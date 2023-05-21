ddecb7ceae9f330f374b6c40c9a28990411d4289
agomezalejo <agomezalejo99@gmail.com>
2023-05-21 20:23:05 -0300

    Ultimas modificaciones para que los ids de clases diferentes sean independientes

--- src/main/java/com/aninfo/model/Account.java
+++ src/main/java/com/aninfo/model/Account.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 public class Account {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long cbu;
 
     private Double balance;
--- src/main/java/com/aninfo/model/Transaction.java
+++ src/main/java/com/aninfo/model/Transaction.java
@@ -10,7 +10,7 @@ public class Transaction {
     }
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Enumerated(EnumType.STRING)
